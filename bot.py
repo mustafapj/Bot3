195c800f9138f06893a63ec2eb6475752b02e406
mustafapj <75590203+mustafapj@users.noreply.github.com>
2025-06-20 07:45:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import os
 from telegram import ReplyKeyboardMarkup, Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     Application,
@@ -10,7 +11,7 @@ from telegram.ext import (
 
 # بيانات المطور
 DEVELOPER_USERNAME = "@pw19k"
-BOT_TOKEN = "7087784225:AAF-TUMXou11lHOr5VLRq37PgCEbOBqKH3U"
+BOT_TOKEN =os.getenv("7087784225:AAF-TUMXou11lHOr5VLRq37PgCEbOBqKH3U") 
 REQUIRED_CHANNEL = "@e2m_2"  # قناة الاشتراك الإجباري
 
 # تعريف الأزرار الرئيسية
