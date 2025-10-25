a174e1fb4a5905453f9712fb16e4e5d62f862d06
mustafapj <75590203+mustafapj@users.noreply.github.com>
2025-10-25 05:33:28 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# config.py
+BOT_TOKEN = "ضع_توكن_البوت_هنا"  # احصل عليه من @BotFather
+API_ID = 1234567                  # احصل عليه من my.telegram.org
+API_HASH = "your_api_hash_here"   # احصل عليه من my.telegram.org
