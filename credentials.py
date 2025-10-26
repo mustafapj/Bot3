f3b10b4bbb62f1fbed8df184f4a3cb3bca2ac090
mustafapj <75590203+mustafapj@users.noreply.github.com>
2025-10-26 07:19:27 +0300

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,6 @@
+# credentials.py
+API_ID = 1234567
+API_HASH = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+BOT_TOKEN = "1234567890:ABCxxxxxxxxxxxxxxxxxxxxxxx"
+DEVELOPER_USERNAME = "MASTFA_20022"
+CHANNEL_USERNAME = "YourChannelName"
