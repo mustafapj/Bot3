1a4347292f0b91a8dd55454b3a48e27a2906fa8a
mustafapj <75590203+mustafapj@users.noreply.github.com>
2025-11-19 19:55:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,1 @@
-# config.py
-
-# 🔐 التوكن والمتغيرات الحساسة
-BOT_TOKEN = "7087784225:AAF-TUMXou11lHOr5VLRq37PgCEbOBqKH3U"  # تأكد أن التوكن مكتمل
-
-# ⚙️ إعدادات القناة والمطور
-CHANNEL_USERNAME = "MASTFA_20022"
-DEVELOPER_USERNAME = "pw19k"
-
-# 🔧 إعدادات الأداء
-READ_TIMEOUT = 30
-WRITE_TIMEOUT = 30
-CONNECT_TIMEOUT = 30
-POOL_TIMEOUT = 30
-
-# 🔗 إعدادات API (إذا كنت تحتاجها)
-API_ID = 26455325
-API_HASH = "c00851db310f6e3cdf29333ef312c219"
+..
